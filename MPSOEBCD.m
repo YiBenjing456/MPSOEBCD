51be50a1412acbaa7291bdfa65a049d3d516e2e4
YiBenjing456 <58563955+YiBenjing456@users.noreply.github.com>
2023-04-12 19:36:40 +0800

    Update MPSOEBCD.m

--- MPSOEBCD.m
+++ MPSOEBCD.m
@@ -1,19 +1,5 @@
 function MPSOEBCD(Global)
 % <algorithm> <M>
-% Large-scale multi-objective competitive swarm optimization algorithm
-
-%------------------------------- Reference --------------------------------
-% Y. Tian, X. Zheng, X. Zhang, and Y. Jin, Efficient large-scale
-% multi-objective optimization based on a competitive swarm optimizer, IEEE
-% Transactions on Cybernetics, 2019.
-%------------------------------- Copyright --------------------------------
-% Copyright (c) 2018-2019 BIMK Group. You are free to use the PlatEMO for
-% research purposes. All publications which use this platform or any code
-% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
-% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
-% for evolutionary multi-objective optimization [educational forum], IEEE
-% Computational Intelligence Magazine, 2017, 12(4): 73-87".
-%--------------------------------------------------------------------------
 
     %% Generate random population
     IGDTrace = [];
